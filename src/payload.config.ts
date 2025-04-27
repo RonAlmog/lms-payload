438f8081e9e5945294f1787b7375ba9dbf353c20
Ron Almog <ron8132@gmail.com>
2025-04-27 09:54:35 -0400

    add linode or s3 bucket storage

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -9,6 +9,8 @@ import sharp from 'sharp'
 
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
+// if you want to use S3 or linode
+import { s3Storage } from '@payloadcms/storage-s3'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -32,6 +34,18 @@ export default buildConfig({
   sharp,
   plugins: [
     payloadCloudPlugin(),
-    // storage-adapter-placeholder
+    // storage adapter settings
+    s3Storage({
+      collections: { media: true },
+      bucket: process.env.S3_BUCKET_NAME || '',
+      config: {
+        region: process.env.S3_REGION || '',
+        endpoint: process.env.S3_ENDPOINT || '',
+        credentials: {
+          accessKeyId: process.env.S3_ACCESS_KEY || '',
+          secretAccessKey: process.env.S3_SECRET_KEY || '',
+        },
+      },
+    }),
   ],
 })
