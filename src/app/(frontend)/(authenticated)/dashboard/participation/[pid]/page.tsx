faf8c4b9bb2493a3635c4f312db77221db2cf71e
Ron Almog <ron8132@gmail.com>
2025-04-30 17:09:43 -0400

    add course participation page

---
+++ src/app/(frontend)/(authenticated)/dashboard/participation/[pid]/page.tsx
@@ -0,0 +1,39 @@
+import { Participation } from '@/payload-types'
+import { getUser } from '../../../actions/get-user'
+import { getPayload } from 'payload'
+import configPromise from '@payload-config'
+import { notFound } from 'next/navigation'
+
+interface Props {
+  params: {
+    pid: string
+  }
+}
+export default async function ParticipationPage({ params }: Props) {
+  const { pid } = await params
+  console.log({ pid })
+  const payload = await getPayload({ config: configPromise })
+  const user = await getUser()
+
+  let participation: Participation | null = null
+
+  try {
+    const res: Participation = await payload.findByID({
+      collection: 'participation',
+      id: pid,
+      overrideAccess: false,
+      user,
+    })
+
+    participation = res
+  } catch (error) {
+    console.error(error)
+    return notFound()
+  }
+
+  if (!participation) {
+    return notFound()
+  }
+
+  return <div>{participation.course.title}</div>
+}
