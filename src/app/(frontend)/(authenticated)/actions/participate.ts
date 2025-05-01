8a73da80b3b38b058156ede242a39f7f37e4a65b
Ron Almog <ron8132@gmail.com>
2025-05-01 07:56:07 -0400

    add participate action

---
+++ src/app/(frontend)/(authenticated)/actions/participate.ts
@@ -0,0 +1,36 @@
+'use server';
+
+import { headers as getHeaders } from 'next/headers';
+import { getPayload } from 'payload';
+import configPromise from '@payload-config';
+import type { Payload } from 'payload';
+import { getUser } from "./get-user";
+
+export async function participate({ courseId }: { courseId: string; }) {
+    const headers = await getHeaders();
+    const payload: Payload = await getPayload({ config: await configPromise });
+    const user = await getUser();
+
+    if (!user) {
+        throw new Error('User not found');
+    }
+
+    try {
+        const createdParticipation = await payload.create({
+            collection: 'participation',
+            data: {
+                course: courseId,
+                customer: user.id,
+                progress: 0
+            },
+            overrideAccess: false,
+            user
+        });
+
+        return createdParticipation;
+    } catch (error) {
+        console.error(error);
+        throw new Error('Error creating participation');
+    }
+
+}
